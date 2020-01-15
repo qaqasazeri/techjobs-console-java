6e7e33cddddfd4c940c14b305f8b1fe770a6a950
Fagan Safarov <fagansafarov1985@gmail.com>
2020-01-14 21:23:38 -0600

    assignment1

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -57,12 +57,12 @@ public class JobData {
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param column Column that should be searched.
+     * @param value  Value of teh field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -80,7 +80,24 @@ public class JobData {
                 jobs.add(row);
             }
         }
+        System.out.println("No records Found");
+        return jobs;
+    }
 
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> job : allJobs) {
+            for (String key : job.keySet()) {
+                if (job.get(key).contains(value)) {
+                    jobs.add(job);
+
+                }
+            }
+        }
+        System.out.println("No records Found");
         return jobs;
     }
 
